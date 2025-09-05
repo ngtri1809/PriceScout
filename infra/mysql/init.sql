06ad143c12d4f8c21bca16b715c9e7d5c7947a36
Tri Nguyen <ngtri1809@gmail.com>
2025-09-05 16:36:57 -0700

    delete unused

--- infra/mysql/init.sql
+++
@@ -1,8 +0,0 @@
--- Initialize MySQL database for PriceScout
-CREATE DATABASE IF NOT EXISTS pricescout;
-USE pricescout;
-
--- Create user for the application
-CREATE USER IF NOT EXISTS 'pricescout'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON pricescout.* TO 'pricescout'@'%';
-FLUSH PRIVILEGES;
